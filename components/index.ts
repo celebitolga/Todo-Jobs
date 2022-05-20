5789270633432d5b6d9f380dedd789d4e22f57df
tolgacelebi <tolga@artistanbul.io>
2022-05-20 19:15:24 +0300

    components/index.ts created and components will export from this file

---
+++ components/index.ts
@@ -0,0 +1,9 @@
+export { default as Badge } from "./Badge";
+export { default as Button } from "./Button";
+export { default as Footer } from "./Footer";
+export { default as Header } from "./Header";
+export { default as Input } from "./Input";
+export { default as Label } from "./Label";
+export { default as Layout } from "./Layout";
+export { default as Select } from "./Select";
+export { default as Todo } from "./Todo";
